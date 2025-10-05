85518a8f16a2acbd9bb65a421e1503f145dff58c
user <user@192.168.1.5>
2025-10-05 22:44:53 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main(){
+  print(sayHello('gali'));
+
+  var costomer = Customer();
+  var product = Produck();
+  var category = Category();
+}
